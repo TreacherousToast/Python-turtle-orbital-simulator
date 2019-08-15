3b142a26718cbff82b34256679a75f9882824853
TreacherousToast <callumoriley@gmail.com>
2019-08-15 08:25:55 -0700

    Update orbitSimRS.py

--- orbitSimRS.py
+++ orbitSimRS.py
@@ -5,15 +5,15 @@ import math
 
 t = turtle.Turtle()
 # parameters
-x = 10 # x value of the spaceship. Has to be greater than 0, or else the program will crash from the first calculation.
+x = 0.1 # x value of the spaceship. Has to be greater than 0, or else the program will crash from the first calculation.
 y = 160 # y value of the spaceship
-dx = 18 # x velocity (delta-x)
+dx = 5 # x velocity (delta-x)
 dy = 0 # y velocity (delta-y)
 ax = 0 # x acceleration
 ay = 0 # y acceleration
 pr = 80 # planet radius
 pm = 6.67*10**16 # planet mass
-scale = 100
+scale = 1000 # see function above. Essentially saying 1 turtle unit = scale meters
 
 t.penup() # draw planet
 t.goto(0,-pr)
@@ -25,10 +25,10 @@ t.pendown()
 
 while True:
   theta = math.atan(abs(y/x))
-  g = -(6.67*10**-11)*pm/(x**2+y**2) # newton's law of gravitation modified for calculating gravitaitional field strength, with G=1
+  g = -(6.67*10**-11)*pm/(x**2+y**2) # newton's law of gravitation modified for calculating gravitaitional field strength
   ax = abs(g*math.cos(theta)) # calculating magnitude of accelerations in each direction
   ay = abs(g*math.sin(theta))
-  ax /= scale
+  ax /= scale # adjusting scale
   ay /= scale
   
   if x > 0 and y > 0: # changing direction of acceleration depending on orientation relative to planet
